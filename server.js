bbfce20c25477c845cec63628e860d13d3b610a4
umitao <umitao@gmail.com>
2020-09-30 15:09:58 +0300

    fixed tsquery

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.use(cors());
 const pool = require("./db");
 const authorization = require("./middleware/authorization");
 const jwt = require("jsonwebtoken");
+const tsquery = require("pg-tsquery")(/* options can be passed to override the defaults */);
 require("dotenv").config();
 
 // READ BODIES AND URL FROM REQUESTS
@@ -23,7 +24,7 @@ app.use("/auth", require("./routes/jwtAuth"));
 app.use("/profile", require("./routes/profile"));
 
 //ADD A BOOK
-app.post("/book",authorization, function (req, res) {
+app.post("/book", authorization, function (req, res) {
   // const jwtToken = req.header("token");
   // const payload = jwt.verify(jwtToken, process.env.jwtSecret);
   // req.user = payload.user;
@@ -86,35 +87,33 @@ app.delete("/delete", function (req, res) {
   console.log(q);
 
   let query =
-      "WITH deletebook AS (DELETE FROM users_vs_books uvb WHERE books_id = $1 RETURNING books_id) DELETE FROM books b WHERE id = (SELECT * FROM deletebook)";
+    "WITH deletebook AS (DELETE FROM users_vs_books uvb WHERE books_id = $1 RETURNING books_id) DELETE FROM books b WHERE id = (SELECT * FROM deletebook)";
 
   pool
     .query(query, [q])
     .then(() => res.status(200).send(`Book with ID=${q} deleted`))
     .catch((error) => {
       console.log(error);
-      res.status(500).send("Something went wrong :( ...");  
+      res.status(500).send("Something went wrong :( ...");
     });
 });
 
 //SEARCHING WITH TSQUERY - INDEXING & TRIGGERS LACKING ON DB
 app.get("/search", function (req, res) {
-  const { q : searchTerm } = req.query;
-  const regexSearch = searchTerm.replace(/\b\s/g, ":* | " );//faced some problem with the tsquery on this line please recheck with master branch
+  const { q: searchTerm } = req.query;
 
   let query =
     "SELECT * FROM books WHERE title_tokens || author_tokens || language_tokens @@ to_tsquery($1);";
 
   pool
-    .query(query, [regexSearch])
+    .query(query, [tsquery(searchTerm)])
     .then((result) => res.json(result.rows))
     .catch((err) => console.error(err));
 });
 
-
 //all for display try
 app.get("/search", function (req, res) {
-  const { q : searchTerm } = req.query;
+  // const { q: searchTerm } = req.query;
   let query = "SELECT * FROM books limit 60";
 
   pool
@@ -136,7 +135,7 @@ app.get("/userpage", authorization, function (req, res) {
 
 //GET ALL BOOKS for homescreen.................................................................................................
 app.get("/books", function (req, res) {
-  let query = "SELECT * FROM books limit 60";
+  let query = "SELECT * FROM books limit 40";
 
   pool
     .query(query)
@@ -144,12 +143,11 @@ app.get("/books", function (req, res) {
     .catch((err) => console.error(err));
 });
 
-
-//get requests of the user 
+//get requests of the user
 app.get("/:id/request", function (req, res) {
   const { id } = req.params;
 
-  let query = 
+  let query =
     "SELECT * FROM books b JOIN users_vs_books uvb ON b.id = uvb.books_id WHERE uvb.users_id = $1;";
 
   pool
@@ -162,7 +160,7 @@ app.get("/:id/request", function (req, res) {
 app.get("/:id/books", function (req, res) {
   const { id } = req.params;
 
-  let query = 
+  let query =
     "SELECT * FROM books b JOIN users_vs_books uvb ON b.id = uvb.books_id WHERE uvb.users_id = $1;";
 
   pool
@@ -174,7 +172,8 @@ app.get("/:id/books", function (req, res) {
 //SINGLE BOOK PAGE QUERY
 app.get("/book", function (req, res) {
   const bookId = req.query.q;
-  let query = "select * FROM books b join users_vs_books uvb on b.id = uvb.books_id WHERE b.id = $1";
+  let query =
+    "select * FROM books b join users_vs_books uvb on b.id = uvb.books_id WHERE b.id = $1";
 
   pool
     .query(query, [bookId])
@@ -183,22 +182,25 @@ app.get("/book", function (req, res) {
 });
 
 // REQUEST BOOK..........................................................................................................................
-app.post("/requestbook",(req, res) => {
+app.post("/requestbook", (req, res) => {
   // console.log(req.user);
 
-   const {bookId, userId} = req.body;
-   console.log(userId);
+  const { bookId, userId } = req.body;
+  console.log(userId);
 
-  const query1 = "select users_id from users_vs_books uvb where books_id = $1"
-  const query2 ="INSERT INTO book_requests (requesting_user_id, book_id, owner_id) VALUES ($1, $2, $3) RETURNING * ";
+  const query1 = "select users_id from users_vs_books uvb where books_id = $1";
+  const query2 =
+    "INSERT INTO book_requests (requesting_user_id, book_id, owner_id) VALUES ($1, $2, $3) RETURNING * ";
 
   pool
     .query(query1, [bookId])
     .then((result) => {
       const ownerId = result.rows[0].users_id;
-      pool.query(query2, [userId, bookId, ownerId]).then(() => res.status(200).send("Book Requested")).catch(err => console.error(err))
-
-      })
+      pool
+        .query(query2, [userId, bookId, ownerId])
+        .then(() => res.status(200).send("Book Requested"))
+        .catch((err) => console.error(err));
+    })
     .catch((err) => {
       if (err.code === "23503") {
         res.status(400).send("Book does not exist.");
@@ -219,41 +221,40 @@ app.post("/requestbook",(req, res) => {
 
 //request status of a particular book with owner
 
-app.get("/myBookRequestStatus",(req, res) => {
-   const bookId = req.query.q;
+app.get("/myBookRequestStatus", (req, res) => {
+  const bookId = req.query.q;
 
-  const query1 = "select users_id from users_vs_books uvb where books_id = $1"
-  const query2 = "select users.name,users.email,book_requests.id, book_requests.status from book_requests join users on book_requests.requesting_user_id = users.id join books on book_requests.book_id = books.id where book_requests.owner_id = $1 and books.id = $2 ";
+  const query1 = "select users_id from users_vs_books uvb where books_id = $1";
+  const query2 =
+    "select users.name,users.email,book_requests.id, book_requests.status from book_requests join users on book_requests.requesting_user_id = users.id join books on book_requests.book_id = books.id where book_requests.owner_id = $1 and books.id = $2 ";
 
   pool
     .query(query1, [bookId])
     .then((result) => {
       const ownerId = result.rows[0].users_id;
-      pool.query(query2, [ownerId, bookId]).then((result) => res.status(200).json(result.rows)).catch(err => console.error(err))
-
-      })
+      pool
+        .query(query2, [ownerId, bookId])
+        .then((result) => res.status(200).json(result.rows))
+        .catch((err) => console.error(err));
+    })
     .catch((err) => {
-      
-        console.error(err);
-    
+      console.error(err);
     });
-  }); 
+});
 
-  //accepting the user request for the particular book 
+//accepting the user request for the particular book
 
-  // app.put("/requestUpdate", (req, res) => {
-  //     const requestId =
+// app.put("/requestUpdate", (req, res) => {
+//     const requestId =
 
-
-  // })
-
-
+// })
 
 //request all incoming book request status query............................................................................................
 app.get("/myincomingrequests", function (req, res) {
   const ownerId = req.query.q;
-  const status = "pending"
-  let query = "select users.name,users.email, books.title, book_requests.status from book_requests join users on book_requests.requesting_user_id = users.id join books on book_requests.book_id = books.id where book_requests.owner_id = $1";
+  const status = "pending";
+  let query =
+    "select users.name,users.email, books.title, book_requests.status from book_requests join users on book_requests.requesting_user_id = users.id join books on book_requests.book_id = books.id where book_requests.owner_id = $1";
 
   pool
     .query(query, [ownerId])
@@ -264,8 +265,9 @@ app.get("/myincomingrequests", function (req, res) {
 //request all outgoing book request status query.............................................................................................
 app.get("/myoutgoingrequests", function (req, res) {
   const ownerId = req.query.q;
-  const status = "pending"
-  let query = "select users.name,users.email, books.title, book_requests.status from book_requests join users on book_requests.owner_id = users.id join books on book_requests.book_id = books.id where book_requests.requesting_user_id = $1";
+  const status = "pending";
+  let query =
+    "select users.name,users.email, books.title, book_requests.status from book_requests join users on book_requests.owner_id = users.id join books on book_requests.book_id = books.id where book_requests.requesting_user_id = $1";
 
   pool
     .query(query, [ownerId])
@@ -273,30 +275,29 @@ app.get("/myoutgoingrequests", function (req, res) {
     .catch((err) => console.error(err));
 });
 
-
-
-
 //update status endpoint(accept).............................................................................................
 app.put("/request/accept", function (req, res) {
   const requestId = req.query.q;
-  const query = "UPDATE book_requests SET status = 'accepted' WHERE id = $1 returning id"
+  const query =
+    "UPDATE book_requests SET status = 'accepted' WHERE id = $1 returning id";
 
-  pool.query(query, [requestId]).then((result) => res.status(200).send(result.rows)).catch((err) => console.error(err))
-
-
-})
+  pool
+    .query(query, [requestId])
+    .then((result) => res.status(200).send(result.rows))
+    .catch((err) => console.error(err));
+});
 
 //update status endpoint (reject).............................................................................................
 app.put("/request/reject", function (req, res) {
   const requestId = req.query.q;
-  const query = "UPDATE book_requests SET status = 'rejected' WHERE id = $1 returning id"
+  const query =
+    "UPDATE book_requests SET status = 'rejected' WHERE id = $1 returning id";
 
-  pool.query(query, [requestId]).then((result) => res.status(200).send(result.rows)).catch((err) => console.error(err))
-
-
-})
-
-
+  pool
+    .query(query, [requestId])
+    .then((result) => res.status(200).send(result.rows))
+    .catch((err) => console.error(err));
+});
 
 app.listen(3001, function () {
   console.log("Server is listening on port 3001. Ready to accept requests!");
